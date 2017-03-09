2eac882d4609bb533b3e25197f8c187109d1f6c7
Ian <ian.explore.world@gmail.com>
2017-03-09 21:56:35 +0900

    Add some methods for system tracing

--- checkinfo.sh
+++ checkinfo.sh
@@ -22,3 +22,31 @@ uname -r
 ### Distribution version (Ubuntu)
 cat /etc/lsb-release
 
+#---------------------------------------------------
+
+### Check disk IO
+iostat -x 1
+
+
+### Check disk IO (need to install, more readable)
+nmon 
+
+
+### Check IO 
+blktrace -d $DEV 
+blkparse -i $DEV.blktrace.* > $OUTPUT
+
+# Ex. Trace the IO onto /dev/sda    
+# blkparse -i /dev/sda ; blkparse -i sda.blktrace.* > out.log
+
+
+### Check System call
+strace -p $TARGET_PID
+
+# Ex. Trace the syscall called by mysqld 
+# shell> ps aux | grep mysql[d] | awk '{print $2}' | xargs -i sudo strace -ttt -yp {} -f
+# - grep mysql[d]: grep mysql but ignore grep command itself
+# - xargs -i {}  : direct the output to {} to be an argument of the next command 
+# - strace -f    : see not only the parent process but also the child processes
+
+
