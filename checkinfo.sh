6bdecfe21cf87353aacd63d82b78029ddda633c8
Ian <ian.explore.world@gmail.com>
2017-03-10 18:03:44 +0900

    Check memory usage of a process

--- checkinfo.sh
+++ checkinfo.sh
@@ -24,15 +24,22 @@ cat /etc/lsb-release
 
 #---------------------------------------------------
 
-### Check disk IO
+################
+#Check disk IO
+################
+
 iostat -x 1
 
 
-### Check disk IO (need to install, more readable)
+################
+#Check disk IO (need to install, more readable)
+################
 nmon 
 
 
-### Check IO 
+################
+# Check IO 
+################
 blktrace -d $DEV 
 blkparse -i $DEV.blktrace.* > $OUTPUT
 
@@ -40,7 +47,9 @@ blkparse -i $DEV.blktrace.* > $OUTPUT
 # blkparse -i /dev/sda ; blkparse -i sda.blktrace.* > out.log
 
 
-### Check System call
+################
+# Check System call
+################
 strace -p $TARGET_PID
 
 # Ex. Trace the syscall called by mysqld 
@@ -48,5 +57,18 @@ strace -p $TARGET_PID
 # - grep mysql[d]: grep mysql but ignore grep command itself
 # - xargs -i {}  : direct the output to {} to be an argument of the next command 
 # - strace -f    : see not only the parent process but also the child processes
+
+
+################
+# Check Memory Usage of a process 
+################
+ps aux 
+
+# Ex. Show memory usage of SQLite every 0.05 seconds 
+# shell> ps aux | grep USE[R] ; while sleep 0.05; do ps aux | grep -v vi | grep sqlit[e]; done 
+# - grep USE[R]  : print out the first row 
+# - use while and sleep to achieve every 0.05 seconds
+# - grep -v vi   : not show vi (might be process the sqlite related file)
+
 
 
